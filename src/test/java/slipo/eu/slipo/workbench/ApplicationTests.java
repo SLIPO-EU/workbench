aec589323076d841efb0cdd01062816b2adf49cc
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2017-06-19 13:28:25 +0300

    Create structure for application tests

--- src/test/java/slipo/eu/slipo/workbench/ApplicationTests.java
+++ src/test/java/slipo/eu/slipo/workbench/ApplicationTests.java
@@ -1,4 +1,4 @@
-package slipo.eu.workbench;
+package eu.slipo.workbench;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTests {
-
+@SpringBootTest(properties = {"spring.profiles.active=testing"})
+public class ApplicationTests 
+{
 	@Test
 	public void contextLoads() {
 	}
