31bbde8e1c9b6a84e952b676b215c57fe4099598
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2017-08-24 22:19:03 +0300

    Enhance SubCommand interface (non-option args, short help)

---
+++ command/src/main/java/eu/slipo/workbench/command/SubCommand.java
@@ -0,0 +1,49 @@
+package eu.slipo.workbench.command;
+
+import java.util.Map;
+
+/**
+ * A simple interface for a subcommand.
+ *  
+ * Supports some common forms of command-line (positional and option-like) arguments.
+ */
+public interface SubCommand
+{
+    /**
+     * Provide help on command's syntax. 
+     * 
+     * @return an one-liner summary
+     */
+    String getSummary();
+    
+    /**
+     * Provide a description for the duties of this command.
+     * 
+     * @return
+     */
+    String getDescription();
+    
+    /**
+     * Run with options (no positional arguments).
+     * 
+     * @param options
+     */
+    void run(Map<String, String> options);
+    
+    /**
+     * Run with 1 positional (non-option) argument and a map of options.
+     * 
+     * @param a1 the 1st positional argument
+     * @param options
+     */
+    void run(String a1, Map<String, String> options);
+    
+    /**
+     * Run with 2 positional (non-option) arguments and a map of options.
+     * 
+     * @param a1 the 1st positional argument
+     * @param a2 the 2nd positional argument
+     * @param options
+     */
+    void run(String a1, String a2, Map<String, String> options);
+}
--- command/src/main/java/eu/slipo/workbench/command/subcommand/HelpCommand.java
+++ command/src/main/java/eu/slipo/workbench/command/subcommand/HelpCommand.java
@@ -2,16 +2,17 @@ package eu.slipo.workbench.command.subcommand;
 
 import java.util.Arrays;
 import java.util.Map;
+
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import eu.slipo.workbench.command.Command.SubCommand;
+import eu.slipo.workbench.command.SubCommand;
 
 @Component("help")
 public class HelpCommand implements SubCommand
@@ -21,14 +22,58 @@ public class HelpCommand implements SubCommand
     
     private static Logger logger = LoggerFactory.getLogger(HelpCommand.class);
     
+    private SubCommand subcommandByName(String name)
+    {
+        SubCommand c;
+        try {
+            c = applicationContext.getBean(name, SubCommand.class);
+        } catch (BeansException ex) {
+            c = null;
+        }
+        return c;
+    }
+    
+    private List<String> subcommandNames()
+    {
+        return Arrays.asList(applicationContext.getBeanNamesForType(SubCommand.class));
+    }
+    
     @Override
     public void run(Map<String, String> args)
     {
-        List<String> names = Arrays.asList(
-            applicationContext.getBeanNamesForType(SubCommand.class));
-        
         logger.info(
-            "The following commands are available: {}", 
-            String.join(", ", names));
+            "The following subcommands are available: {}", 
+            String.join(", ", subcommandNames()));
+    }
+
+    @Override
+    public void run(String subcommandName, Map<String, String> options)
+    {
+        // Print help on a given subcommand
+        SubCommand c = subcommandByName(subcommandName);
+        if (c == null) {
+            logger.info("No such command: {}", subcommandName);
+        } else {
+            logger.info("syntax: {}", c.getSummary());
+        }
+    }
+
+    @Override
+    public void run(String subcommandName, String a2, Map<String, String> options)
+    {
+        // ignore any extra non-option arguments, just print help on subcommand
+        run(subcommandName, options);
+    }
+    
+    @Override
+    public String getSummary()
+    {
+        return "help [<subcommand-name>]";
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "List available commands or help on a specific command";
     }
 }
