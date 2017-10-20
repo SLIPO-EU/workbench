a23be37fe78683442f43474fe71b0a4335125482
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2017-10-20 18:32:41 +0300

    Update base response object
    When an error occurs and no valid result exists, the result property is not serialized.

--- common/src/main/java/eu/slipo/workbench/common/model/RestResponse.java
+++ common/src/main/java/eu/slipo/workbench/common/model/RestResponse.java
@@ -1,10 +1,11 @@
 package eu.slipo.workbench.common.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RestResponse<Result>
@@ -12,55 +13,59 @@ public class RestResponse<Result>
     private final List<Error> errors = new ArrayList<Error>();
 
     private final Result result;
-    
-    protected RestResponse() 
+
+    protected RestResponse()
     {
         this.result = null;
     }
-    
+
     protected RestResponse(Result r)
     {
         this.result = r;
     }
-    
-    protected RestResponse(Result r, List<Error> errors) 
+
+    protected RestResponse(Result r, List<Error> errors)
     {
         this.result = r;
         this.errors.addAll(errors);
     }
 
-    public boolean hasErrors() 
-    {        
+    public boolean hasErrors()
+    {
         return errors.size() > 0;
     }
 
-    @JsonProperty("errors")
-    public List<Error> getErrors() 
+    public List<Error> getErrors()
     {
         return Collections.unmodifiableList(errors);
     }
-    
-    @JsonProperty("result")
+
+    @JsonInclude(Include.NON_NULL)
     public Result getResult()
     {
         return result;
     }
-    
+
+    @JsonProperty("success")
+    public boolean getSuccess() {
+        return !this.hasErrors();
+    }
+
     public static <R> RestResponse<R> result(R r)
     {
         return new RestResponse<>(r);
     }
-    
+
     public static <R> RestResponse<R> error(ErrorCode code, String description)
     {
         return RestResponse.<R>error(new Error(code, description));
     }
-    
+
     public static <R> RestResponse<R> error(Error e)
     {
         return new RestResponse<R>(null, Collections.singletonList(e));
     }
-    
+
     public static <R> RestResponse<R> error(List<Error> errors)
     {
         return new RestResponse<R>(null, errors);
