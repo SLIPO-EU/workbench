a041e5ee8353da0ddf56ff657974eba548d0d76c
gkostoulas <georgekostoulas@gmail.com>
2017-11-15 12:12:35 +0200

    Small fix in process explorer

--- webapp/src/main/frontend/js/workbench/components/views/process/execution-details.js
+++ webapp/src/main/frontend/js/workbench/components/views/process/execution-details.js
@@ -23,7 +23,7 @@ export default function ExecutionDetails(props) {
   console.log(props.steps);
   if (props.steps!= null) {
     return props.steps.map(step => (
-      <div className= { step.id}>
+      <div key={step.id}>
         <Row style={{ backgroundColor: '#4682b4', borderColor: '#ffffff' ,}}>
           <ReactCard inverse style={{ marginLeft: 15, marginTop: 5, backgroundColor: '#4682b4', borderColor: '#4682b4'}}>
             <CardTitle>{step.component}</CardTitle>
@@ -34,9 +34,9 @@ export default function ExecutionDetails(props) {
         <hr/>
       </div>
     ));
-}
-else {
-  return (<div>-</div>);
+  }
+  else {
+    return (<div>-</div>);
   }
   
 }
