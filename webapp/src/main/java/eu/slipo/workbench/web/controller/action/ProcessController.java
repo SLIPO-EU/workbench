0a045323cded2fb2b5011ea081176e41bfab1b47
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2018-01-24 15:38:44 +0200

    Update webapp resource/process controllers, services and repositories

--- webapp/src/main/java/eu/slipo/workbench/web/controller/action/ProcessController.java
+++ webapp/src/main/java/eu/slipo/workbench/web/controller/action/ProcessController.java
@@ -1,9 +1,8 @@
 package eu.slipo.workbench.web.controller.action;
 
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,23 +10,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import eu.slipo.workbench.common.model.Error;
 import eu.slipo.workbench.common.model.RestResponse;
-import eu.slipo.workbench.web.model.EnumOperation;
-import eu.slipo.workbench.web.model.EnumTool;
-import eu.slipo.workbench.web.model.QueryPagingOptions;
 import eu.slipo.workbench.web.model.QueryResult;
-import eu.slipo.workbench.web.model.UserInfo;
+import eu.slipo.workbench.web.model.process.EnumProcessTask;
+import eu.slipo.workbench.web.model.process.ProcessCreateRequest;
+import eu.slipo.workbench.web.model.process.ProcessDefinitionView;
+import eu.slipo.workbench.web.model.process.ProcessErrorCode;
+import eu.slipo.workbench.web.model.process.ProcessExecutionQuery;
 import eu.slipo.workbench.web.model.process.ProcessExecutionRecord;
-import eu.slipo.workbench.web.model.process.ProcessExecutionStepRecord;
-import eu.slipo.workbench.web.model.process.ProcessHistoryRecord;
 import eu.slipo.workbench.web.model.process.ProcessQuery;
 import eu.slipo.workbench.web.model.process.ProcessRecord;
+import eu.slipo.workbench.web.model.process.ProcessViewResult;
+import eu.slipo.workbench.web.repository.IProcessRepository;
+import eu.slipo.workbench.web.service.IProcessService;
 
 /**
  * Actions for managing processes
  */
 @RestController
 public class ProcessController {
+
+    @Autowired
+    private IProcessRepository processRepository;
+
+    @Autowired
+    private IProcessService processService;
 
     /**
      * Search for processes
@@ -37,35 +45,35 @@ public class ProcessController {
      * @return a list of processes
      */
     @RequestMapping(value = "/action/process/query", method = RequestMethod.POST, produces = "application/json")
-    public RestResponse<QueryResult<ProcessRecord>> search(Authentication authentication, @RequestBody ProcessQuery data) {
-        QueryPagingOptions pagingOptions = data.getPagingOptions();
-        if (pagingOptions == null) {
-            pagingOptions = new QueryPagingOptions();
-            pagingOptions.pageIndex = 0;
-            pagingOptions.pageSize = 10;
+    public RestResponse<QueryResult<ProcessRecord>> find(Authentication authentication, @RequestBody ProcessQuery query) {
 
-        } else if (pagingOptions.pageIndex < 0) {
-            pagingOptions.pageIndex = 0;
-        } else if (pagingOptions.pageSize <= 0) {
-            pagingOptions.pageSize = 10;
+        if (query == null) {
+            RestResponse.error(ProcessErrorCode.QUERY_IS_EMPTY, "The query is empty");
         }
 
-        int count = (pagingOptions.pageIndex + 1) * pagingOptions.pageSize + (int) (Math.random() * 100);
+        query.setTask(EnumProcessTask.DATA_INTEGRATION);
+        query.setTemplate(false);
 
-        QueryResult<ProcessRecord> result = new QueryResult<ProcessRecord>(pagingOptions, count);
+        QueryResult<ProcessRecord> result = this.processRepository.find(query);
 
-        long id = pagingOptions.pageIndex * pagingOptions.pageSize + 1;
+        return RestResponse.result(result);
+    }
 
-        for (int i = 0; i < pagingOptions.pageSize; i++) {
-            id += i;
-            int versions = (int) (Math.random() * 10F);
+    /**
+     * Search for process executions
+     *
+     * @param authentication the authenticated principal
+     * @param data the query to execute
+     * @return a list of processes
+     */
+    @RequestMapping(value = "/action/process/execution/query", method = RequestMethod.POST, produces = "application/json")
+    public RestResponse<QueryResult<ProcessExecutionRecord>> find(Authentication authentication, @RequestBody ProcessExecutionQuery query) {
 
-            ProcessRecord process = this.createProcess(id, 1);
-            for (int j = 0; j < versions; j++) {
-                process.addVersion(this.createProcessHistory(id, j + 2));
-            }
-            result.addItem(process);
+        if (query == null) {
+            RestResponse.error(ProcessErrorCode.QUERY_IS_EMPTY, "The query is empty");
         }
+
+        QueryResult<ProcessExecutionRecord> result = this.processRepository.find(query);
 
         return RestResponse.result(result);
     }
@@ -80,15 +88,9 @@ public class ProcessController {
      * @return a list of {@link ProcessExecutionRecord}
      */
     @RequestMapping(value = "/action/process/{id}/{version}/execution", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse<List<ProcessExecutionRecord>> getAllExecutions(Authentication authentication, @PathVariable long id, @PathVariable long version) {
-        List<ProcessExecutionRecord> executions = new ArrayList<ProcessExecutionRecord>();
+    public RestResponse<List<ProcessExecutionRecord>> getAllProcessExecutions(Authentication authentication, @PathVariable long id, @PathVariable long version) {
 
-        int count = (int) (Math.random() * 20F);
-        for (int j = 0; j < count; j++) {
-            executions.add(this.createProcessExecution(j + 1, id, version, false));
-        }
-
-        return RestResponse.<List<ProcessExecutionRecord>>result(executions);
+        return RestResponse.<List<ProcessExecutionRecord>>result(this.processService.findExecutions(id, version));
     }
 
     /**
@@ -102,81 +104,66 @@ public class ProcessController {
      * @return a list of {@link ProcessExecutionRecord}
      */
     @RequestMapping(value = "/action/process/{processId}/{processVersion}/execution/{executionId}", method = RequestMethod.GET, produces = "application/json")
-    public RestResponse<ProcessExecutionRecord> getExecution(
+    public RestResponse<ProcessExecutionRecord> getProcessExecution(
             Authentication authentication,
             @PathVariable long processId,
             @PathVariable long processVersion,
             @PathVariable long executionId) {
 
-        return RestResponse.<ProcessExecutionRecord>result(this.createProcessExecution(executionId, processId, processVersion, true));
-    }
-
-    private ProcessRecord createProcess(long id, int version) {
-        ProcessRecord process = new ProcessRecord(id, version);
-
-        process.setCreatedBy(createUser());
-        process.setCreatedOn(ZonedDateTime.now());
-        process.setUpdatedBy(createUser());
-        process.setUpdatedOn(ZonedDateTime.now());
-        process.setName(String.format("Process %d-%d", id, version));
-        process.setDescription("Process description");
-
-        return process;
-    }
-
-    private ProcessHistoryRecord createProcessHistory(long id, int version) {
-        ProcessHistoryRecord process = new ProcessHistoryRecord(id, version);
-
-        process.setUpdatedBy(createUser());
-        process.setUpdatedOn(ZonedDateTime.now());
-        process.setName(String.format("Process %d-%d", id, version));
-        process.setDescription("Process description");
-
-        return process;
-    }
-
-    private ProcessExecutionRecord createProcessExecution(long executionId, long processId, long processVersion, boolean includeSteps) {
-        ProcessExecutionRecord execution = new ProcessExecutionRecord(executionId, processId, processVersion);
-
-        execution.setStatus("COMPLETED");
-        execution.setStartedOn(ZonedDateTime.now().minusDays(10));
-        execution.setCompletedOn(ZonedDateTime.now().minusDays(9));
-
-        if(includeSteps) {
-            this.addSteps(execution);
+        ProcessExecutionRecord record = processRepository.findOne(processId, processVersion, executionId);
+        if (record == null) {
+            return RestResponse.<ProcessExecutionRecord>error(new Error(ProcessErrorCode.NOT_FOUND, "Execution was not found"));
         }
-
-        return execution;
+        return RestResponse.<ProcessExecutionRecord>result(record);
     }
 
-    private void addSteps(ProcessExecutionRecord execution) {
-        ProcessExecutionStepRecord step = new ProcessExecutionStepRecord(0);
+    /**
+     * Loads the most recent version of an existing process
+     *
+     * @param authentication the authenticated principal
+     * @param data the process model
+     * @return the updated process model
+     */
+    @RequestMapping(value = "/action/process/{id}", method = RequestMethod.GET, produces = "application/json")
+    public RestResponse<ProcessViewResult> findOne(Authentication authentication, @PathVariable long id) {
 
-        step.setComponent(EnumTool.LIMES);
-        step.setOperation(EnumOperation.INTERLINK);
-        step.setStartedOn(ZonedDateTime.now().minusHours(10));
-        step.setCompletedOn(ZonedDateTime.now().minusHours(9));
-        execution.addStep(step);
-
-        step = new ProcessExecutionStepRecord(1);
-
-        step.setComponent(EnumTool.FAGI);
-        step.setOperation(EnumOperation.FUSION);
-        step.setStartedOn(ZonedDateTime.now().minusHours(8));
-        step.setCompletedOn(ZonedDateTime.now().minusHours(5));
-        execution.addStep(step);
-
-        step = new ProcessExecutionStepRecord(2);
-
-        step.setComponent(EnumTool.DEER);
-        step.setOperation(EnumOperation.ENRICHEMENT);
-        step.setStartedOn(ZonedDateTime.now().minusHours(4));
-        step.setCompletedOn(ZonedDateTime.now().minusHours(1));
-        execution.addStep(step);
+        ProcessDefinitionView process = this.processService.findOne(id);
+        if(process == null) {
+            return RestResponse.<ProcessViewResult>error(new Error(ProcessErrorCode.NOT_FOUND, "Process was not found"));
+        }
+        return RestResponse.<ProcessViewResult>result(new ProcessViewResult(process));
     }
 
-    private UserInfo createUser() {
-        return new UserInfo(1, "Administrator");
+    /**
+     * Loads the specific version of an existing process
+     *
+     * @param authentication the authenticated principal
+     * @param data the process model
+     * @return the updated process model
+     */
+    @RequestMapping(value = "/action/process/{id}/{version}", method = RequestMethod.GET, produces = "application/json")
+    public RestResponse<ProcessViewResult> findOne(Authentication authentication, @PathVariable long id, @PathVariable long version) {
+
+        ProcessDefinitionView process = this.processService.findOne(id, version);
+        if(process == null) {
+            return RestResponse.<ProcessViewResult>error(new Error(ProcessErrorCode.NOT_FOUND, "Process was not found"));
+        }
+        return RestResponse.<ProcessViewResult>result(new ProcessViewResult(process));
+    }
+
+    /**
+     * Creates/Updates a new/existing process
+     *
+     * @param authentication the authenticated principal
+     * @param data the process model
+     * @return the updated process model
+     */
+    @RequestMapping(value = "/action/process", method = RequestMethod.POST, produces = "application/json")
+    public RestResponse<ProcessViewResult> update(Authentication authentication, @RequestBody ProcessCreateRequest request) {
+
+        List<Error> errors = this.processService.update(request.getProcess());
+
+        return RestResponse.<ProcessViewResult>error(errors);
     }
 
 }
--- webapp/src/main/java/eu/slipo/workbench/web/controller/action/ResourceController.java
+++ webapp/src/main/java/eu/slipo/workbench/web/controller/action/ResourceController.java
@@ -3,13 +3,13 @@ package eu.slipo.workbench.web.controller.action;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.time.ZonedDateTime;
 import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,21 +22,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import eu.slipo.workbench.common.model.BasicErrorCode;
 import eu.slipo.workbench.common.model.RestResponse;
-import eu.slipo.workbench.web.model.EnumDataFormat;
-import eu.slipo.workbench.web.model.EnumResourceType;
-import eu.slipo.workbench.web.model.QueryPagingOptions;
 import eu.slipo.workbench.web.model.QueryResult;
-import eu.slipo.workbench.web.model.process.ProcessDesignerUpdateModel;
-import eu.slipo.workbench.web.model.process.ProcessDesignerUpdateModelBuilder;
+import eu.slipo.workbench.web.model.process.ProcessDefinitionUpdate;
+import eu.slipo.workbench.web.model.process.ProcessDefinitionUpdateBuilder;
 import eu.slipo.workbench.web.model.resource.EnumDataSource;
 import eu.slipo.workbench.web.model.resource.RegistrationRequest;
 import eu.slipo.workbench.web.model.resource.ResourceErrorCode;
 import eu.slipo.workbench.web.model.resource.ResourceMetadataUpdate;
-import eu.slipo.workbench.web.model.resource.ResourceMetadataView;
 import eu.slipo.workbench.web.model.resource.ResourceQuery;
 import eu.slipo.workbench.web.model.resource.ResourceRecord;
 import eu.slipo.workbench.web.model.resource.ResourceRegistrationRequest;
 import eu.slipo.workbench.web.model.resource.UploadDataSource;
+import eu.slipo.workbench.web.repository.IResourceRepository;
 
 /**
  * Actions for managing resources
@@ -46,8 +43,12 @@ public class ResourceController {
 
     private static final Logger logger = LoggerFactory.getLogger(ResourceController.class);
 
+    @Autowired
+    private IResourceRepository resourceRepository;
+
     /**
-     * Folder where temporary files are saved. This folder must be accessible from the SLIPO service
+     * Folder where temporary files are saved. This folder must be accessible from the
+     * SLIPO service
      */
     @Value("${working.directory}")
     private String workingDirectory;
@@ -60,35 +61,13 @@ public class ResourceController {
      * @return a list of resources
      */
     @RequestMapping(value = "/action/resource/query", method = RequestMethod.POST, produces = "application/json")
-    public RestResponse<QueryResult<ResourceRecord>> search(Authentication authentication, @RequestBody ResourceQuery data) {
-        QueryPagingOptions pagingOptions = data.getPagingOptions();
-        if (pagingOptions == null) {
-            pagingOptions = new QueryPagingOptions();
-            pagingOptions.pageIndex = 0;
-            pagingOptions.pageSize = 10;
+    public RestResponse<QueryResult<ResourceRecord>> find(Authentication authentication, @RequestBody ResourceQuery query) {
 
-        } else if (pagingOptions.pageIndex < 0) {
-            pagingOptions.pageIndex = 0;
-        } else if (pagingOptions.pageSize <= 0) {
-            pagingOptions.pageSize = 10;
+        if (query == null) {
+            RestResponse.error(ResourceErrorCode.QUERY_IS_EMPTY, "The query is empty");
         }
 
-        int count = (pagingOptions.pageIndex + 1) * pagingOptions.pageSize + (int) (Math.random() * 100);
-
-        QueryResult<ResourceRecord> result = new QueryResult<ResourceRecord>(pagingOptions, count);
-
-        long id = pagingOptions.pageIndex + pagingOptions.pageSize + 1;
-
-        for (int i = 0; i < pagingOptions.pageSize; i++) {
-            id += i;
-            int versions = (int) (Math.random() * 10F);
-
-            ResourceRecord resource = this.createResource(id, 1);
-            for (int j = 0; j < versions; j++) {
-                resource.addVersion(this.createResource(id, j + 2));
-            }
-            result.addItem(resource);
-        }
+        QueryResult<ResourceRecord> result = this.resourceRepository.find(query);
 
         return RestResponse.result(result);
     }
@@ -104,15 +83,15 @@ public class ResourceController {
     public RestResponse<?> registerResource(Authentication authentication, @RequestBody ResourceRegistrationRequest request) {
         // Action does not support file uploading
         if (request.getDataSource().getType() == EnumDataSource.UPLOAD) {
-            return RestResponse.error(ResourceErrorCode.DATASOURCE_NOT_SUPPORTED, "Data source of type 'UPLOAD' is not supported.");
+            return RestResponse.error(ResourceErrorCode.DATASOURCE_NOT_SUPPORTED,
+                    "Data source of type 'UPLOAD' is not supported.");
         }
 
         // Convert registration data to a process configuration instance
-        ProcessDesignerUpdateModel process =  ProcessDesignerUpdateModelBuilder
-            .create()
-            .transform(request.getDataSource(), request.getSettings(), 1)
-            .register(request.getMetadata(), 1)
-            .build();
+        ProcessDefinitionUpdate process = ProcessDefinitionUpdateBuilder.create()
+                .transform(0, "Transform", request.getDataSource(), request.getSettings(), 1)
+                .register(1, "Register", request.getMetadata(), 1)
+                .build();
 
         // TODO: Submit request to service
 
@@ -128,25 +107,22 @@ public class ResourceController {
      * @return
      */
     @RequestMapping(value = "/action/resource/upload", method = RequestMethod.PUT, produces = "application/json")
-    public RestResponse<?> uploadResource(
-            Authentication authentication,
-            @RequestPart("file") MultipartFile file,
+    public RestResponse<?> uploadResource(Authentication authentication, @RequestPart("file") MultipartFile file,
             @RequestPart("data") RegistrationRequest request) {
         try {
             // Create a temporary file
-            final String inputFile = createTemporaryFilename(file.getBytes(), FilenameUtils.getExtension(file.getOriginalFilename()));
+            final String inputFile = createTemporaryFilename(file.getBytes(),
+                    FilenameUtils.getExtension(file.getOriginalFilename()));
 
             // Convert registration data to a process configuration instance
-            ProcessDesignerUpdateModel process =  ProcessDesignerUpdateModelBuilder
-                .create()
-                .transform(new UploadDataSource(inputFile), request.getSettings(), 1)
-                .register(request.getMetadata(), 1)
-                .build();
+            ProcessDefinitionUpdate process = ProcessDefinitionUpdateBuilder.create()
+                    .transform(0, "Transform", new UploadDataSource(inputFile), request.getSettings(), 1)
+                    .register(1, "Register", request.getMetadata(), 1).build();
 
-            //  TODO: Submit request to service
+            // TODO: Submit request to service
 
             return RestResponse.result(process);
-        } catch(IOException ex) {
+        } catch (IOException ex) {
             logger.error(ex.getMessage(), ex);
 
             return RestResponse.error(BasicErrorCode.IO_ERROR, "IO exception has occured: " + ex.getMessage());
@@ -162,13 +138,7 @@ public class ResourceController {
      */
     @RequestMapping(value = "/action/resource/{id}", method = RequestMethod.GET, produces = "application/json")
     public RestResponse<ResourceRecord> getResource(Authentication authentication, @PathVariable long id) {
-        ResourceRecord resource = this.createResource(id, 1);
-
-        resource.addVersion(this.createResource(id, 2));
-        resource.addVersion(this.createResource(id, 3));
-        resource.addVersion(this.createResource(id, 4));
-
-        return RestResponse.<ResourceRecord>result(resource);
+        return RestResponse.<ResourceRecord>result(this.resourceRepository.findOne(id));
     }
 
     /**
@@ -179,14 +149,9 @@ public class ResourceController {
      * @return the updated resource metadata
      */
     @RequestMapping(value = "/action/resource/{id}", method = RequestMethod.POST, produces = "application/json")
-    public RestResponse<?> updateResource(Authentication authentication,  @PathVariable long id, @RequestBody ResourceMetadataUpdate data) {
-        ResourceRecord resource = this.createResource(id, 1, data);
+    public RestResponse<?> updateResource(Authentication authentication, @PathVariable long id, @RequestBody ResourceMetadataUpdate data) {
 
-        resource.addVersion(this.createResource(id, 2));
-        resource.addVersion(this.createResource(id, 3));
-        resource.addVersion(this.createResource(id, 4));
-
-        return RestResponse.<ResourceRecord>result(resource);
+        return RestResponse.<ResourceRecord>result(this.resourceRepository.findOne(id));
     }
 
     /**
@@ -210,7 +175,8 @@ public class ResourceController {
      * @return an instance {@link
      */
     @RequestMapping(value = "/action/resource/{id}/{version}", method = RequestMethod.DELETE, produces = "application/json")
-    public RestResponse<?> deleteResource(Authentication authentication, @PathVariable long id, @PathVariable int version) {
+    public RestResponse<?> deleteResource(Authentication authentication, @PathVariable long id,
+            @PathVariable int version) {
         return RestResponse.result(null);
     }
 
@@ -232,45 +198,6 @@ public class ResourceController {
         FileUtils.writeByteArrayToFile(new File(filename), data);
 
         return filename;
-    }
-
-    private ResourceRecord createResource(long id, int version) {
-        return this.createResource(id, version, null);
-    }
-
-    private ResourceRecord createResource(long id, int version, ResourceMetadataUpdate metadata) {
-        ResourceRecord resource = new ResourceRecord(id, version);
-
-        resource.setType(EnumResourceType.POI_DATA);
-        resource.setDataSource(EnumDataSource.UPLOAD);
-        resource.setInputFormat(EnumDataFormat.GPX);
-        resource.setOutputFormat(EnumDataFormat.N_TRIPLES);
-        resource.setProcessExecutionId(1L);
-        resource.setCreatedOn(ZonedDateTime.now());
-        resource.setUpdatedOn(resource.getCreatedOn());
-        resource.setTable(UUID.randomUUID());
-        resource.setFileName("file.xml");
-        resource.setFileSize((int) (Math.random() * 1024 * 1024) + 100);
-
-        if (metadata != null) {
-            resource.setMetadata(
-                new ResourceMetadataView(
-                    metadata.getName(),
-                    metadata.getDescription(),
-                    (int) (Math.random() * 100 + 100)
-                )
-            );
-        } else {
-            resource.setMetadata(
-                new ResourceMetadataView(
-                    String.format("Resource %d", id),
-                    "Uploaded sample POI data",
-                    (int) (Math.random() * 100 + 100)
-                )
-            );
-        }
-
-        return resource;
     }
 
 }
