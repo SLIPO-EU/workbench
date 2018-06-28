9e7a50d37f1a61f8d9a70b85a5b8ceff11524059
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2018-06-28 12:37:40 +0300

    [webapp] Remove unused methods

--- webapp/src/main/frontend/js/workbench/util/index.js
+++ webapp/src/main/frontend/js/workbench/util/index.js
@@ -1,7 +1,4 @@
-export { default as diffShallow } from './diff-shallow';
 export { default as formatFileSize } from './file-size';
-export { default as mirrorToPath } from './path-mirror';
-export { default as randomString } from './random-string';
 export { default as validateUrl } from './validate-url';
 
 import {
--- webapp/src/main/frontend/js/workbench/util/random-string.js
+++
@@ -1,3 +0,0 @@
-module.exports = function () {
-  return parseInt(Math.random() * 1e+9).toString(36);
-};
