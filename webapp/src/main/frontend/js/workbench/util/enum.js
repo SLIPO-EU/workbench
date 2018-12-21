fcf87f2aaf4056e4da3812085dbde6395cbacbf9
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2018-12-21 10:45:08 +0200

    [web] Update helper methods
    Added DOM helper methods addClass, removeClass.
    Removed enumeration helper method isDefined.

--- webapp/src/main/frontend/js/workbench/util/enum.js
+++
@@ -1,7 +0,0 @@
-function isDefined(type, value) {
-  return (!!Object.keys(type).find((key) => type[key] === value));
-}
-
-export {
-  isDefined,
-};
--- webapp/src/main/frontend/js/workbench/util/index.js
+++ webapp/src/main/frontend/js/workbench/util/index.js
@@ -1,14 +1,7 @@
 export { kpiDataToSeriesByPrefix } from './chart';
 export { checkStatus } from './check-fetch-status';
 export { checkError } from './check-json';
+export { addClass, removeClass } from './dom';
 export { formatFileSize } from './file-size';
 export { toFeatureCollection } from './geojson';
 export { validateUrl } from './validate-url';
-
-import {
-  isDefined,
-} from './enum';
-
-export const Enum = {
-  isDefined,
-};
