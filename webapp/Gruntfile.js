25b59ce880a3f08d9473df63c41f9e854d0b5cb2
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2019-05-06 17:38:33 +0300

    [webapp] Fix Grunt build
    Current grunt-contrib-uglify plugin does not support es6.

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -317,7 +317,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-eslint');
