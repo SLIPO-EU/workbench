bd911d11a5e58f5eda9926e529d3fdf0bf540648
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2019-12-12 18:33:50 +0200

    Update versions for tools

--- common/src/main/java/eu/slipo/workbench/common/model/tool/DeerConfiguration.java
+++ common/src/main/java/eu/slipo/workbench/common/model/tool/DeerConfiguration.java
@@ -34,7 +34,7 @@ public class DeerConfiguration extends EnrichConfiguration<Deer>
     /**
      * This class represents the configuration of a specific version
      */
-    public static final String VERSION = "2.1";
+    public static final String VERSION = "2.2";
 
     /**
      * Available configuration levels
--- common/src/main/java/eu/slipo/workbench/common/model/tool/ReverseTriplegeoConfiguration.java
+++ common/src/main/java/eu/slipo/workbench/common/model/tool/ReverseTriplegeoConfiguration.java
@@ -41,7 +41,7 @@ public class ReverseTriplegeoConfiguration extends TransformConfiguration<Revers
     /**
      * This class represents the configuration of a specific version
      */
-    public static final String VERSION = "1.8";
+    public static final String VERSION = "2.0";
 
     /**
      * The output name (without any file extension)
